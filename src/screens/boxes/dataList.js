d0a94f460d7b571b67ddb1fd7b0c1c2f4f08bcab
miran-thaqi <miranthaqi12@gmail.com>
2022-03-07 00:47:21 +0300

    init

---
+++ src/screens/boxes/dataList.js
@@ -0,0 +1,264 @@
+
+
+export default  [
+  {
+    firstName:'Miran',
+    lastName:'Thaqi',
+    middleName:'Faik',
+    photoUrl:'https://raw.githubusercontent.com/pixelastic/fakeusers/master/pictures/algolia/men/honza.png',
+    address: {
+      street: "Kamer Thaqi",
+      suite: "Apt. 692",
+      city: "Drenas",
+      zipcode: "1300",
+    },
+  },
+  {
+    firstName:'Muadh',
+    lastName:'Thaqi',
+    middleName:'Faik',
+    photoUrl: 'https://raw.githubusercontent.com/pixelastic/fakeusers/master/pictures/algolia/men/jeremy.png',
+    address: {
+      street: "Kamer Thaqi",
+      suite: "Apt. 692",
+      city: "Drenas",
+      zipcode: "1300",
+    }
+  },
+  {
+    firstName:'Miran',
+    lastName:'Thaqi',
+    middleName:'Faik',
+    photoUrl: 'https://raw.githubusercontent.com/pixelastic/fakeusers/master/pictures/algolia/men/julien.png',
+    address: {
+      street: "Kamer Thaqi",
+      suite: "Apt. 692",
+      city: "Drenas",
+      zipcode: "1300",
+    }
+  },
+  {
+    firstName:'Miran',
+    lastName:'Thaqi',
+    middleName:'Faik',
+    photoUrl: 'https://raw.githubusercontent.com/pixelastic/fakeusers/master/pictures/algolia/men/olivier.png',
+    address: {
+      street: "Kamer Thaqi",
+      suite: "Apt. 692",
+      city: "Drenas",
+      zipcode: "1300",
+    }
+  },
+  {
+    firstName:'Miran',
+    lastName:'Thaqi',
+    middleName:'Faik',
+    photoUrl: 'https://raw.githubusercontent.com/pixelastic/fakeusers/master/pictures/algolia/men/xander.png',
+    address: {
+      street: "Kamer Thaqi",
+      suite: "Apt. 692",
+      city: "Drenas",
+      zipcode: "1300",
+    }
+  },
+  {
+    firstName:'Miran',
+    lastName:'Thaqi',
+    middleName:'Faik',
+    photoUrl: 'https://raw.githubusercontent.com/pixelastic/fakeusers/master/pictures/algolia/men/julien.png',
+    address: {
+      street: "Kamer Thaqi",
+      suite: "Apt. 692",
+      city: "Drenas",
+      zipcode: "1300",
+    }
+  },
+  {
+    firstName:'Miran',
+    lastName:'Thaqi',
+    middleName:'Faik',
+    photoUrl: 'https://raw.githubusercontent.com/pixelastic/fakeusers/master/pictures/algolia/men/olivier.png',
+    address: {
+      street: "Kamer Thaqi",
+      suite: "Apt. 692",
+      city: "Drenas",
+      zipcode: "1300",
+    }
+  },
+  {
+    firstName:'Miran',
+    lastName:'Thaqi',
+    middleName:'Faik',
+    photoUrl: 'https://raw.githubusercontent.com/pixelastic/fakeusers/master/pictures/algolia/men/xander.png',
+    address: {
+      street: "Kamer Thaqi",
+      suite: "Apt. 692",
+      city: "Drenas",
+      zipcode: "1300",
+    }
+  },
+  {
+    firstName:'Miran',
+    lastName:'Thaqi',
+    middleName:'Faik',
+    photoUrl: 'https://raw.githubusercontent.com/pixelastic/fakeusers/master/pictures/algolia/men/julien.png',
+    address: {
+      street: "Kamer Thaqi",
+      suite: "Apt. 692",
+      city: "Drenas",
+      zipcode: "1300",
+    }
+  },
+  {
+    firstName:'Miran',
+    lastName:'Thaqi',
+    middleName:'Faik',
+    photoUrl: 'https://raw.githubusercontent.com/pixelastic/fakeusers/master/pictures/algolia/men/olivier.png',
+    address: {
+      street: "Kamer Thaqi",
+      suite: "Apt. 692",
+      city: "Drenas",
+      zipcode: "1300",
+    }
+  },
+  {
+    firstName:'Miran',
+    lastName:'Thaqi',
+    middleName:'Faik',
+    photoUrl: 'https://raw.githubusercontent.com/pixelastic/fakeusers/master/pictures/algolia/men/xander.png',
+    address: {
+      street: "Kamer Thaqi",
+      suite: "Apt. 692",
+      city: "Drenas",
+      zipcode: "1300",
+    }
+  },
+  {
+    firstName:'Miran',
+    lastName:'Thaqi',
+    middleName:'Faik',
+    photoUrl: 'https://raw.githubusercontent.com/pixelastic/fakeusers/master/pictures/algolia/men/julien.png',
+    address: {
+      street: "Kamer Thaqi",
+      suite: "Apt. 692",
+      city: "Drenas",
+      zipcode: "1300",
+    }
+  },
+  {
+    firstName:'Miran',
+    lastName:'Thaqi',
+    middleName:'Faik',
+    photoUrl: 'https://raw.githubusercontent.com/pixelastic/fakeusers/master/pictures/algolia/men/olivier.png',
+    address: {
+      street: "Kamer Thaqi",
+      suite: "Apt. 692",
+      city: "Drenas",
+      zipcode: "1300",
+    }
+  },
+
+  {
+    firstName:'Miran',
+    lastName:'Thaqi',
+    middleName:'Faik',
+    photoUrl: 'https://raw.githubusercontent.com/pixelastic/fakeusers/master/pictures/algolia/men/olivier.png',
+    address: {
+      street: "Kamer Thaqi",
+      suite: "Apt. 692",
+      city: "Drenas",
+      zipcode: "1300",
+    }
+  },
+
+  {
+    firstName:'Miran',
+    lastName:'Thaqi',
+    middleName:'Faik',
+    photoUrl: 'https://raw.githubusercontent.com/pixelastic/fakeusers/master/pictures/algolia/men/olivier.png',
+    address: {
+      street: "Kamer Thaqi",
+      suite: "Apt. 692",
+      city: "Drenas",
+      zipcode: "1300",
+    }
+  },
+
+  {
+    firstName:'Miran',
+    lastName:'Thaqi',
+    middleName:'Faik',
+    photoUrl: 'https://raw.githubusercontent.com/pixelastic/fakeusers/master/pictures/algolia/men/olivier.png',
+    address: {
+      street: "Kamer Thaqi",
+      suite: "Apt. 692",
+      city: "Drenas",
+      zipcode: "1300",
+    }
+  },
+  {
+    firstName:'Miran',
+    lastName:'Thaqi',
+    middleName:'Faik',
+    photoUrl: 'https://raw.githubusercontent.com/pixelastic/fakeusers/master/pictures/algolia/men/olivier.png',
+    address: {
+      street: "Kamer Thaqi",
+      suite: "Apt. 692",
+      city: "Drenas",
+      zipcode: "1300",
+    }
+  },
+
+  {
+    firstName:'Miran',
+    lastName:'Thaqi',
+    middleName:'Faik',
+    photoUrl: 'https://raw.githubusercontent.com/pixelastic/fakeusers/master/pictures/algolia/men/olivier.png',
+    address: {
+      street: "Kamer Thaqi",
+      suite: "Apt. 692",
+      city: "Drenas",
+      zipcode: "1300",
+    }
+  },
+
+  {
+    firstName:'Miran',
+    lastName:'Thaqi',
+    middleName:'Faik',
+    photoUrl: 'https://raw.githubusercontent.com/pixelastic/fakeusers/master/pictures/algolia/men/olivier.png',
+    address: {
+      street: "Kamer Thaqi",
+      suite: "Apt. 692",
+      city: "Drenas",
+      zipcode: "1300",
+    }
+  },
+
+  {
+    firstName:'Miran',
+    lastName:'Thaqi',
+    middleName:'Faik',
+    photoUrl: 'https://raw.githubusercontent.com/pixelastic/fakeusers/master/pictures/algolia/men/olivier.png',
+    address: {
+      street: "Kamer Thaqi",
+      suite: "Apt. 692",
+      city: "Drenas",
+      zipcode: "1300",
+    }
+  },
+
+  {
+    firstName:'Miran',
+    lastName:'Thaqi',
+    middleName:'Faik',
+    photoUrl: 'https://raw.githubusercontent.com/pixelastic/fakeusers/master/pictures/algolia/men/olivier.png',
+    address: {
+      street: "Kamer Thaqi",
+      suite: "Apt. 692",
+      city: "Drenas",
+      zipcode: "1300",
+    }
+  },
+
+]
